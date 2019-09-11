6ed75f59deb3bd441fd8032b9f253c1e038cd496
Joe Mansfield <joe.mansfield@sureskills.com>
2019-09-11 12:29:41 +0100

    Interim fix for CloudPub sub lab
    Reverting subscription functions to resolve issues in testing. This still uses the new style object setup for the framework but leaves subscriptions working as before.

--- developingapps_v1.2/nodejs/pubsub-languageapi-spanner/bonus/server/gcp/pubsub.js
+++ developingapps_v1.2/nodejs/pubsub-languageapi-spanner/bonus/server/gcp/pubsub.js
@@ -27,7 +27,7 @@ function publishFeedback(feedback) {
 
 
 function registerFeedbackNotification(cb) {
-  feedbackTopic.createSubscription('feedback-subscription', { autoAck: true })
+  feedbackTopic.subscribe('feedback-subscription', { autoAck: true })
     .then(results => {
       const subscription = results[0];
 
@@ -43,7 +43,7 @@ function registerFeedbackNotification(cb) {
 }
 
 function registerAnswerNotification(cb) {
-  answersTopic.createSubscription('answer-subscription', { autoAck: true })
+  answersTopic.subscribe('answer-subscription', { autoAck: true })
     .then(results => {
       const subscription = results[0];
 
