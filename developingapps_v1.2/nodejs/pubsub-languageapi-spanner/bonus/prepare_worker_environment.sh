92d8054221ed23de47cc5e34559af89b5850fbce
AtishUtturkar <atish.utturkar@sureskills.com>
2019-09-19 17:00:24 +0100

    Lab5(Javascript)UpdateNew 1
    Lab5(Javascript)Update prepare_worker_environment.sh.
    
    Subscription

--- developingapps_v1.2/nodejs/pubsub-languageapi-spanner/bonus/prepare_worker_environment.sh
+++ developingapps_v1.2/nodejs/pubsub-languageapi-spanner/bonus/prepare_worker_environment.sh
@@ -30,6 +30,10 @@ echo "Creating Cloud Pub/Sub topics"
 gcloud pubsub topics create feedback
 gcloud pubsub topics create answers
 
+echo "Creating Cloud Pub/Sub Subscriptions"
+gcloud pubsub subscriptions create feedback-subscription cloud-shell-subscription --topic feedback
+gcloud pubsub subscriptions create answer-subscription cloud-shell-subscription --topic answers
+
 echo "Creating Cloud Spanner Instance, Database, and Tables"
 gcloud spanner instances create quiz-instance --config=regional-us-central1 --description="Quiz instance" --nodes=1
 gcloud spanner databases create quiz-database --instance quiz-instance --ddl "CREATE TABLE Feedback ( feedbackId STRING(100) NOT NULL, email STRING(100), quiz STRING(20), feedback STRING(MAX), rating INT64, score FLOAT64, timestamp INT64 ) PRIMARY KEY (feedbackId); CREATE TABLE Answers (answerId STRING(100) NOT NULL, id INT64, email STRING(60), quiz STRING(20), answer INT64, correct INT64, timestamp INT64) PRIMARY KEY (answerId DESC);"
